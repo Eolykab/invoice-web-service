2d99914839d21adb4f8b97e02c14dbbccf64231f
Eljoenai Muninga <Eolykab@yahoo.co.za>
2018-08-15 15:51:34 +0200

    Added some tests

--- src/main/java/za/co/digitalplatoon/invoiceservice/invoice/Invoice.java
+++ src/main/java/za/co/digitalplatoon/invoiceservice/invoice/Invoice.java
@@ -19,7 +19,7 @@ import javax.persistence.OneToMany;
  */
 @Entity
 public class Invoice {
-    
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "invoice_id")
@@ -30,63 +30,63 @@ public class Invoice {
     @OneToMany(cascade = CascadeType.ALL)
     @JoinColumn(name = "invoice_id", referencedColumnName = "invoice_id")
     private List<LineItem> lineItems;
-    
+
     public long getId() {
         return id;
     }
-    
+
     public void setId(long id) {
         this.id = id;
     }
-    
+
     public String getClient() {
         return client;
     }
-    
+
     public void setClient(String client) {
         this.client = client;
     }
-    
+
     public List<LineItem> getLineItems() {
         return lineItems;
     }
-    
+
     public void setLineItems(List<LineItem> lineItems) {
         this.lineItems = lineItems;
     }
-    
+
     public long getVatRate() {
         return vatRate;
     }
-    
+
     public void setVatRate(long vatRate) {
         this.vatRate = vatRate;
     }
-    
+
     public Date getInvoiceDate() {
         return invoiceDate;
     }
-    
+
     public void setInvoiceDate(Date invoiceDate) {
         this.invoiceDate = invoiceDate;
     }
-    
+
     public BigDecimal getSubTotal() {
-        
+
         double total = 0;
         for (LineItem item : getLineItems()) {
             total += item.getQuantity() * item.getUnitPrice().doubleValue();
         }
         return new BigDecimal(total).setScale(2, RoundingMode.HALF_UP);
-        
+
     }
-    
+
     public BigDecimal getVat() {
         return new BigDecimal(getVatRate() * getSubTotal().doubleValue()).setScale(2, RoundingMode.HALF_UP);
     }
-    
+
     public BigDecimal getTotal() {
         return new BigDecimal(getSubTotal().doubleValue() + getVat().doubleValue()).setScale(2, RoundingMode.HALF_UP);
     }
-    
+
 }
--- src/test/java/za/co/digitalplatoon/invoiceservice/invoice/InvoiceWebServiceApplicationTests.java
+++ src/test/java/za/co/digitalplatoon/invoiceservice/invoice/InvoiceWebServiceApplicationTests.java
@@ -1,16 +1,46 @@
 package za.co.digitalplatoon.invoiceservice.invoice;
 
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.BDDMockito;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class InvoiceWebServiceApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @MockBean
+    private InvoiceController invoiceController;
+
+    @Test
+    public void viewAllInvoices() throws Exception {
+        Invoice invoice = new Invoice();
+        invoice.setClient("Eljoenai Muninga");
+        invoice.setInvoiceDate(new Date());
+        List<Invoice> invoices = Collections.singletonList(invoice);
+        BDDMockito.given(invoiceController.viewAllInvoices()).willReturn(invoices);
+    }
+
+    @Test
+    public void viewInvoice() throws Exception {
+        Invoice invoice = new Invoice();
+        invoice.setClient("Eljoenai Muninga");
+        invoice.setInvoiceDate(new Date());
+        BDDMockito.given(invoiceController.viewInvoice(invoice.getId())).willReturn(invoice);
+
+    }
+
+    @Test
+    public void addInvoice() throws Exception {
+        Invoice invoice = new Invoice();
+        invoice.setClient("Eljoenai Muninga");
+        invoice.setInvoiceDate(new Date());
+        BDDMockito.given(invoiceController.addInvoice(invoice)).willReturn(invoice);
+    }
 
 }
